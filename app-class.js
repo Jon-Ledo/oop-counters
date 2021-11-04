c08e7b08c8d2c789d799a72cebac3a01acfc438c
JonLedo <jon.ledo@hotmail.com>
2021-11-04 10:37:01 -0400

    converted to class to demonstrate difference

---
+++ app-class.js
@@ -0,0 +1,45 @@
+// function to generate a selector
+function getElement (selection) {
+  const element = document.querySelector(selection)
+  if (element) {
+    return element
+  }
+  throw new Error (`Please check "${selection}" selector, no such element exists`)
+}
+
+class Counter {
+  constructor (element, value) {
+    this.counter = element
+    this.value = value
+    
+    // selectors
+    this.valueNum = element.querySelector('.value')
+    this.resetBtn = element.querySelector('.reset')
+    this.increaseBtn = element.querySelector('.increase')
+    this.decreaseBtn = element.querySelector('.decrease')
+
+    // set starting count
+    this.valueNum.textContent = this.value
+
+    // left of the dot is the button, so we need to redirect what this should point to (counter) using .bind(this)
+    this.resetBtn.addEventListener('click', this.reset.bind(this))
+    this.increaseBtn.addEventListener('click', this.increase.bind(this))
+    this.decreaseBtn.addEventListener('click', this.decrease.bind(this))
+  }
+  increase() {
+    this.value++
+    this.valueNum.textContent = this.value
+  }
+  decrease() {
+    this.value--
+    this.valueNum.textContent = this.value
+  }
+  reset() {
+    this.value = 0
+    this.valueNum.textContent = this.value
+  }
+}
+
+
+const firstCounter = new Counter(getElement('.first-counter'), 100)
+const secondCounter = new Counter(getElement('.second-counter'), 200)
